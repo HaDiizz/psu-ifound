c4480edc695f3feee65af41e6ebbfff9e98fd0fb
HaDiizz <nattapol.sh@hotmail.com>
2024-04-18 20:47:50 +0700

    Fixed layout render ErrorBanned

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,26 +91,28 @@ export const metadata = {
 
 export default async function RootLayout({ children }) {
   const session = await getServerSession(authOptions);
-  if (session && session.user) {
-    if (session.user.status === "INACTIVE") {
-      return <ErrorBanned />;
-    }
-  }
+
   return (
     <html suppressHydrationWarning={true} lang="en">
       <body className={inter.className}>
-        <ProgressLoader />
-        <NextTopLoader showSpinner={false} color="#f564ce" />
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-        <Providers session={session}>
-          <Navbar />
-          <Toaster position="bottom-left" reverseOrder={false} />
-          <main className="w-full min-h-screen overflow-hidden bg-cover bg-gradient-to-tr from-gray-100 to-indigo-50 dark:bg-gradient-to-tr dark:from-gray-900 dark:to-slate-900">
-            {children}
-          </main>
-        </Providers>
+        {session && session.user && session.user.status === "INACTIVE" ? (
+          <ErrorBanned />
+        ) : (
+          <>
+            <ProgressLoader />
+            <NextTopLoader showSpinner={false} color="#f564ce" />
+            <div className="main">
+              <div className="gradient"></div>
+            </div>
+            <Providers session={session}>
+              <Navbar />
+              <Toaster position="bottom-left" reverseOrder={false} />
+              <main className="w-full min-h-screen overflow-hidden bg-cover bg-gradient-to-tr from-gray-100 to-indigo-50 dark:bg-gradient-to-tr dark:from-gray-900 dark:to-slate-900">
+                {children}
+              </main>
+            </Providers>
+          </>
+        )}
       </body>
     </html>
   );
